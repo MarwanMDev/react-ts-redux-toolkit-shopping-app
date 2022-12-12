3c02d75be7596684f273982b436ff70461abe121
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-12-12 23:14:51 +0200

    landing page

--- src/components/molecules/slider/index.tsx
+++ src/components/molecules/slider/index.tsx
@@ -2,8 +2,12 @@ import { Swiper, SwiperProps } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper.min.css';
 import 'swiper/swiper-bundle.css';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+
 import React from 'react';
 import { NavigationButton } from '../../atoms';
+import { EffectFade } from 'swiper';
 
 type SliderProps = {
   children: React.ReactNode | JSX.Element;
@@ -25,9 +29,16 @@ const Slider = ({
     swiperSlide.slidePrev();
   };
   return (
-    <div className="px-10 w-full relative">
-      <div className={className}>{title}</div>
-      <Swiper {...rest} onSwiper={(e: any) => setSwiperSlide(e)}>
+    <div className={`px-10 w-full relative ${className}`}>
+      {title && (
+        <div className="text-2xl my-16 font-bold">{title}</div>
+      )}
+      <Swiper
+        {...rest}
+        onSwiper={(e: any) => setSwiperSlide(e)}
+        modules={[Navigation, Pagination, EffectFade]}
+        effect="fade"
+      >
         <NavigationButton type="prev" onClick={onPrev} />
         {children}
         <NavigationButton type="next" onClick={onNext} />
